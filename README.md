ab33554f038d4cd50be80525d9cf8f7bcda6e506
Paiman Rasoli <paimanrasoli789@gmail.com>
2023-10-04 00:15:41 +0430

    create django app and create media_service

---
+++ README.md
@@ -0,0 +1,1 @@
+Django app and Kong CDN prototype demo app.
---
+++ cdn_integration_kong/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cdn_integration_kong project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdn_integration_kong.settings')
+
+application = get_asgi_application()
