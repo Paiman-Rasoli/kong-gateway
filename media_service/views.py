649b7cb28e147940326f00744c400682cd1d7c2e
Paiman Rasoli <paimanrasoli789@gmail.com>
2023-10-04 05:35:33 +0430

    use uuid as file name

--- media_service/views.py
+++ media_service/views.py
@@ -1,3 +1,6 @@
+'''
+Note: In this assessment I focused on Kong and File manipulation. not so much on Django code style! 
+'''
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -6,6 +9,7 @@ from django.views.decorators.http import require_POST
 from media_service.models import MediaModel
 import os
 import io
+import uuid
 
 
 
@@ -20,11 +24,16 @@ def upload(request):
             return JsonResponse({"message" : "file_path is required" , "status" : 400})
 
       if isExist(file_path):
-            file_name = os.path.basename(file_path).split(".")[0]
+            file_name = str(uuid.uuid4())
             new_media = MediaModel(name=file_name)
             new_media.image.save(file_name, get_file_data(file_path))
             new_media.save()
-            return JsonResponse({"status" : 200 , "imageURL" : new_media.image.url , "imageName" : file_name})
+            response = {
+              "status" : 200,
+              "imageURL" : new_media.image.url,
+              "imageName" : file_name
+            }
+            return JsonResponse(response)
       else:
             error = {
                   "status" : 400,
@@ -38,7 +47,11 @@ def retrieve(request):
             return JsonResponse({"status" : 400 , "message" : "File Name is required"})
       try:
             find = MediaModel.objects.get(name=file_name)
-            return JsonResponse({"status" : 200 , "fileURL" : find.image.url})
+            response = {
+                  "status" : 200 ,
+                  "fileURL" : find.image.url
+            }
+            return JsonResponse(response)
       except Exception as e:
             print(e)
             return JsonResponse({"status" : 404 , "message" : "image not found"})
